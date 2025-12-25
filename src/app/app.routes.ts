bbca03dd21d8c1c4d7765a5186329c7f4344ba98
Theun van der Wiel <git@van-der-wiel.net>
2025-12-25 21:09:30 +0100

    game setup logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Game } from './controllers/game/game';
+import { Home } from './controllers/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Home },
+	{ path: 'game/:gameId', component: Game },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.less'
+	selector: 'app-root',
+	imports: [
+		RouterOutlet
+	],
+	templateUrl: './app.html',
+	styleUrl: './app.less'
 })
 export class App {
-  protected readonly title = signal('5dChess');
+	protected readonly title = signal('5dChess');
 }
